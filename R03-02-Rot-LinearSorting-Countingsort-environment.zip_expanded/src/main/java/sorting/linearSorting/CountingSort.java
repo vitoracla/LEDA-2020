67c37fc28f3053917637ccfeb2f418744d4cffdc
Vitoracla <vitor.aquino@ccc.ufcg.edu.br>
2020-10-05 14:23:22 -0300

    iniciando o R03

---
+++ R03-02-Rot-LinearSorting-Countingsort-environment.zip_expanded/src/main/java/sorting/linearSorting/CountingSort.java
@@ -0,0 +1,21 @@
+package sorting.linearSorting;
+
+import sorting.AbstractSorting;
+
+/**
+ * Classe que implementa a estratégia de Counting Sort vista em sala. Procure
+ * evitar desperdicio de memoria alocando o array de contadores com o tamanho
+ * sendo o máximo inteiro presente no array a ser ordenado.
+ *
+ * Voce pode assumir que o maior inteiro armazenado não chega a 100.
+ *
+ */
+public class CountingSort extends AbstractSorting<Integer> {
+
+	@Override
+	public void sort(Integer[] array, int leftIndex, int rightIndex) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+}
