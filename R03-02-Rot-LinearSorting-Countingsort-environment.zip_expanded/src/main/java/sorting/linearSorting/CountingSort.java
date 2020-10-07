5c53a879d55ad3af83a860c20287dd5115bb5db8
Vitoracla <vitor.aquino@ccc.ufcg.edu.br>
2020-10-07 16:05:43 -0300

    couting feito

--- R03-02-Rot-LinearSorting-Countingsort-environment.zip_expanded/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-02-Rot-LinearSorting-Countingsort-environment.zip_expanded/src/main/java/sorting/linearSorting/CountingSort.java
@@ -14,8 +14,45 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (array.length <= leftIndex || array == null) {
+			return;
+		}
+
+		int maior = maiorElemento(array, leftIndex, rightIndex);
+		Integer[] aux = new Integer[maior + 1];
+		
+		//transformando elementos nulls em 0
+		for(int o = leftIndex; o < aux.length; o++) {
+			aux[o] = 0;
+		}
+		// fazendo a contagem
+		for(int j = leftIndex; j <= rightIndex; j++) {
+			aux[array[j]]++;
+		}
+		// acumulando
+		for(int k = leftIndex + 1; k < aux.length; k++) {
+			aux[k] += aux[k - 1];
+		}
+		
+		Integer[] ordenado = new Integer[array.length];
+		for(int m = rightIndex; m >= leftIndex; m--) {
+			aux[array[m]]--;
+			ordenado[aux[array[m]]] = array[m];
+		}
+		
+		for(int n = leftIndex; n <= rightIndex; n++) {
+			array[n] = ordenado[n];
+		}
+		
 	}
 
+	private int maiorElemento(Integer[] array, int leftIndex, int rightIndex) {
+		int maior = 0;
+		for (int i = 0; i <= rightIndex; i++) {
+			if (array[i] > maior) {
+				maior = array[i];
+			}
+		}
+		return maior;
+	}
 }
