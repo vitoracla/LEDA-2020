b437933c780249487b84a9449e0239e2dfe5bc08
Vitoracla <vitor.aquino@ccc.ufcg.edu.br>
2020-10-31 11:18:55 -0300

    faltando Stack e Queue

--- R06-02-Rot-Linked-list-environment.zip_expanded/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ R06-02-Rot-Linked-list-environment.zip_expanded/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -1,26 +1,193 @@
 package adt.linkedList;
 
-public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
-		DoubleLinkedList<T> {
+public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements DoubleLinkedList<T> {
 
 	protected DoubleLinkedListNode<T> last;
+	protected int size;
+
+	@Override
+	public T search(T element) {
+
+		DoubleLinkedListNode<T> auxHead = (DoubleLinkedListNode<T>) this.head;
+		DoubleLinkedListNode<T> auxLast = this.last;
+
+		T data = null;
+
+		while (!auxHead.equals(auxLast) && !auxHead.getData().equals(auxLast) && !auxHead.getData().equals(element)
+				&& !auxLast.getData().equals(element)) {
+			auxHead = (DoubleLinkedListNode<T>) auxHead.getNext();
+			auxLast = auxLast.getPrevious();
+		}
+
+		if (auxHead.getData().equals(element)) {
+			data = auxHead.getData();
+		}
+
+		if (auxLast.getData().equals(element)) {
+			data = auxLast.getData();
+		}
+
+		return data;
+
+	}
+
+	@Override
+	public void insert(T element) {
+
+		this.size++;
+
+		if (isEmpty()) {
+
+			DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>();
+
+			newNode.setData(element);
+			newNode.setNext(new DoubleLinkedListNode<T>());
+			newNode.setPrevious(new DoubleLinkedListNode<T>());
+			this.head = newNode;
+			this.last = newNode;
+
+		} else {
+
+			DoubleLinkedListNode<T> auxLast = this.last;
+			DoubleLinkedListNode<T> auxLast2 = null;
+
+			while (!auxLast.isNIL()) {
+				auxLast2 = auxLast;
+				auxLast = (DoubleLinkedListNode<T>) auxLast.getNext();
+			}
+
+			auxLast.setData(element);
+			auxLast.setNext(new DoubleLinkedListNode<T>());
+			auxLast.setPrevious(auxLast2);
+
+			this.last = auxLast;
+		}
+
+	}
+
+	@Override
+	public void remove(T element) {
+
+		DoubleLinkedListNode<T> aux;
+
+		if (this.head.getData().equals(element)) {
+
+			this.size--;
+
+			aux = (DoubleLinkedListNode<T>) this.head.getNext();
+			aux.setPrevious(new DoubleLinkedListNode<>());
+
+			this.head = aux;
+
+		}
+
+		else if (this.last.getData().equals(element)) {
+
+			this.size--;
+
+			aux = (DoubleLinkedListNode<T>) this.last.getPrevious();
+			aux.setNext(new DoubleLinkedListNode<>());
+
+			this.last = aux;
+
+		} else {
+
+			DoubleLinkedListNode<T> auxHead = (DoubleLinkedListNode<T>) this.head;
+			DoubleLinkedListNode<T> auxLast = this.last;
+
+			while (!auxHead.equals(auxLast) && !auxHead.getData().equals(auxLast) && !auxHead.getData().equals(element)
+					&& !auxLast.getData().equals(element)) {
+				auxHead = (DoubleLinkedListNode<T>) auxHead.getNext();
+				auxLast = auxLast.getPrevious();
+			}
+
+			if (auxHead.getData().equals(element)) {
+
+				this.size--;
+
+				DoubleLinkedListNode<T> auxHeadPrev = auxHead.getPrevious();
+				DoubleLinkedListNode<T> auxHeadNext = (DoubleLinkedListNode<T>) auxHead.getNext();
+
+				auxHeadPrev.setNext(auxHeadNext);
+				auxHeadNext.setPrevious(auxHeadPrev);
+				return;
+
+			}
+
+			if (auxLast.getData().equals(element)) {
+
+				this.size--;
+
+				DoubleLinkedListNode<T> auxLastPrev = auxLast.getPrevious();
+				DoubleLinkedListNode<T> auxLastNext = (DoubleLinkedListNode<T>) auxHead.getNext();
+
+				auxLastNext.setPrevious(auxLastPrev);
+				auxLastPrev.setNext(auxLastNext);
+				return;
+			}
+
+		}
+
+	}
 
 	@Override
 	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		DoubleLinkedListNode<T> aux;
+		DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>();
+
+		newNode.setData(element);
+		newNode.setPrevious(new DoubleLinkedListNode<T>());
+
+		this.size++;
+
+		if (isEmpty()) {
+			newNode.setNext(new DoubleLinkedListNode<T>());
+			newNode.setPrevious(new DoubleLinkedListNode<T>());
+			this.head = newNode;
+			this.last = newNode;
+
+		} else {
+			aux = (DoubleLinkedListNode<T>) this.head;
+			newNode.setNext(aux);
+			aux.setPrevious(newNode);
+			this.head = newNode;
+		}
+
 	}
 
 	@Override
 	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty()) {
+			this.size--;
+			DoubleLinkedListNode<T> aux;
+
+			aux = (DoubleLinkedListNode<T>) this.head.getNext();
+			aux.setPrevious(new DoubleLinkedListNode<>());
+
+			this.head = aux;
+
+		}
 	}
 
 	@Override
 	public void removeLast() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty()) {
+
+			if (this.size == 1) {
+				removeFirst();
+				return;
+			}
+
+			this.size--;
+			DoubleLinkedListNode<T> aux;
+
+			aux = (DoubleLinkedListNode<T>) this.last.getPrevious();
+			aux.setNext(new DoubleLinkedListNode<>());
+
+			this.last = aux;
+		}
+
 	}
 
 	public DoubleLinkedListNode<T> getLast() {
@@ -29,6 +196,10 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 
 	public void setLast(DoubleLinkedListNode<T> last) {
 		this.last = last;
+	}
+
+	public int size() {
+		return this.size;
 	}
 
 }
--- R06-02-Rot-Linked-list-environment.zip_expanded/src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
+++ R06-02-Rot-Linked-list-environment.zip_expanded/src/main/java/adt/linkedList/RecursiveDoubleLinkedListImpl.java
@@ -1,7 +1,6 @@
 package adt.linkedList;
 
-public class RecursiveDoubleLinkedListImpl<T> extends
-		RecursiveSingleLinkedListImpl<T> implements DoubleLinkedList<T> {
+public class RecursiveDoubleLinkedListImpl<T> extends RecursiveSingleLinkedListImpl<T> implements DoubleLinkedList<T> {
 
 	protected RecursiveDoubleLinkedListImpl<T> previous;
 
@@ -9,22 +8,92 @@ public class RecursiveDoubleLinkedListImpl<T> extends
 
 	}
 
+	public RecursiveDoubleLinkedListImpl(T data, RecursiveSingleLinkedListImpl<T> next,
+			RecursiveDoubleLinkedListImpl<T> previous) {
+		
+		this.previous = previous;
+	}
+
 	@Override
 	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (element != null) {
+			if (isEmpty()) {
+				this.insert(element);
+			} else {
+				RecursiveDoubleLinkedListImpl<T> newNode = new RecursiveDoubleLinkedListImpl<T>(getData(), getNext(),
+						this);
+				((RecursiveDoubleLinkedListImpl<T>) getNext()).setPrevious(newNode);
+				setNext(newNode);
+				setData(element);
+			}
+		}
 	}
 
 	@Override
 	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		if (!isEmpty()) {
+			if (getPrevious().isEmpty()) {
+
+				setData(getNext().getData());
+				setNext(getNext().getNext());
+				if (getNext() != null)
+					((RecursiveDoubleLinkedListImpl<T>) getNext()).setPrevious(this);
+
+			} else
+				getPrevious().removeFirst();
+		}
 	}
 
 	@Override
 	public void removeLast() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!isEmpty()) {
+			if (getNext().isEmpty()) {
+
+				setData(null);
+				setNext(null);
+				if (getPrevious().isEmpty())
+					setPrevious(null);
+
+			} else
+				((RecursiveDoubleLinkedListImpl<T>) getNext()).removeLast();
+		}
+	}
+
+	@Override
+	public void insert(T element) {
+
+		if (element != null) {
+			if (isEmpty()) {
+				setData(element);
+				setNext(new RecursiveDoubleLinkedListImpl<T>(null, null, this));
+				if (getPrevious() == null)
+					setPrevious(new RecursiveDoubleLinkedListImpl<T>(null, this, null));
+			} else {
+				next.insert(element);
+			}
+		}
+	}
+
+	@Override
+	public void remove(T element) {
+		if (element != null) {
+			if (!isEmpty()) {
+				if (getData().equals(element)) {
+					if (getPrevious().isEmpty() && getNext().isEmpty()) {
+						setData(null);
+						setNext(null);
+						setPrevious(null);
+					} else {
+						setData(next.getData());
+						setNext(next.next);
+						if (getNext() != null)
+							((RecursiveDoubleLinkedListImpl<T>) getNext()).setPrevious(this);
+					}
+				} else
+					next.remove(element);
+			}
+		}
 	}
 
 	public RecursiveDoubleLinkedListImpl<T> getPrevious() {
